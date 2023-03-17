f48a24987a85c8a7ba61f420cb33659105157f68
SouravBandyopadhyay <souravb.1998@gmail.com>
2023-03-17 14:22:14 +0530

    Image_gallery

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['pbs.twimg.com'],
+    domains: ['images.pexels.com'],
   },
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -30,10 +30,13 @@ type Image = {
 
 export default function Gallery({ images }: { images: Image[] }) {
   return (
-    <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+    <div className="mx-auto max-w-2xl  py-5  px-4 lg:max-w-7xl lg:px-8">
+      <p className="animate-text bg-gradient-to-r  from-teal-500 via-purple-500 to-orange-500   bg-clip-text p-5 text-center text-4xl   font-black capitalize text-transparent">
+        Image Gallery
+      </p>
       <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         {images.map((image) => (
-          <BlurImage key={image.id} image={image} />
+          <Modal key={image.id} image={image} />
         ))}
       </div>
     </div>
@@ -61,7 +64,98 @@ function BlurImage({ image }: { image: Image }) {
         />
       </div>
       <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
-      <p className="mt-1 text-lg font-medium text-gray-900">{image.username}</p>
+      <p className="mt-1 text-lg font-medium capitalize text-gray-900">
+        {image.username}
+      </p>
     </a>
+  )
+}
+
+import React from 'react'
+import Link from 'next/link'
+
+export function Modal({ image }: { image: Image }) {
+  const [showModal, setShowModal] = useState(false)
+  const [isLoading, setLoading] = useState(true)
+  return (
+    <>
+      <div>
+        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+          <Image
+            alt=""
+            src={image.imageSrc}
+            layout="fill"
+            objectFit="cover"
+            className={cn(
+              'duration-700 ease-in-out group-hover:opacity-75',
+              isLoading
+                ? 'scale-110 blur-2xl grayscale'
+                : 'scale-100 blur-0 grayscale-0'
+            )}
+            onLoadingComplete={() => setLoading(false)}
+            onClick={() => setShowModal(true)}
+          />
+        </div>
+        <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
+        <p className="mt-1 text-lg font-medium capitalize text-gray-900">
+          {image.username}
+        </p>
+      </div>
+      {showModal ? (
+        <>
+          <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
+            <div className="relative my-6 mx-auto w-auto max-w-3xl">
+              {/*content*/}
+              <div className="relative flex w-full flex-col rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between rounded-t border-b border-solid border-slate-200 p-5">
+                  <h3 className="w-full text-center text-2xl font-semibold capitalize">
+                    {image.username}
+                  </h3>
+                </div>
+                {/*body*/}
+                <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+                  <Image
+                    alt=""
+                    src={image.imageSrc}
+                    layout="fill"
+                    objectFit="cover"
+                    className={cn(
+                      'duration-700 ease-in-out group-hover:opacity-75',
+                      isLoading
+                        ? 'scale-110 blur-2xl grayscale'
+                        : 'scale-100 blur-0 grayscale-0'
+                    )}
+                    onLoadingComplete={() => setLoading(false)}
+                    onClick={() => setShowModal(true)}
+                  />
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end rounded-b border-t border-solid border-slate-200 p-6">
+                  <button
+                    className="background-transparent mr-1 mb-1 px-6 py-2 text-sm font-bold uppercase text-red-500 outline-none transition-all duration-150 ease-linear focus:outline-none"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <Link href={image.href}>
+                    <a target="_blank">
+                      <button
+                        className="mr-1 mb-1 rounded bg-emerald-500 px-6 py-3 text-sm font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-emerald-600"
+                        type="button"
+                      >
+                        Open Link
+                      </button>
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
+        </>
+      ) : null}
+    </>
   )
 }
